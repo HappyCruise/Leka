9e69e0203291f4aba7c93871fd788043b3fa594e
Leevi Laukkanen <laukkanen.leevi@gmail.com>
2021-12-02 10:05:22 +0200

    Added meleecheck variables

--- Assets/MeleeCheck.cs
+++ Assets/MeleeCheck.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeCheck : MonoBehaviour
-{
+{ 
+    //Flags for moving direction
+    [HideInInspector]
+    public float xDir = 0;
+
+    [HideInInspector]
+    public float yDir = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
